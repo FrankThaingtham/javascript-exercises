c01f563d0ea2addc381facb355c9d86b294926e7
FrankThaingtham <123851541+FrankThaingtham@users.noreply.github.com>
2025-01-02 22:08:57 -0500

    05 complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(start, finish) {
+    if (!Number.isInteger(start) || !Number.isInteger(finish)) return "ERROR";
+    if (start < 0 || finish < 0) return "ERROR";
+    if (start > finish) {
+    const temp = start;
+    start = finish;
+    finish = temp;
+  }
+    let solution = 0;
+    for (let i = start; i <= finish; i++) {
+        solution += i;
+    }
+    return solution;
 };
 
 // Do not edit below this line
