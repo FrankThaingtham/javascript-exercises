96526a774a9d7224bdcb169fd2f344daa0a264aa
FrankThaingtham <123851541+FrankThaingtham@users.noreply.github.com>
2025-01-02 20:38:26 -0500

    03 complete

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    const wordArr = word.split("");
+    let reverseWord = "";
+    for (let i = wordArr.length - 1; i >= 0; i--) {
+        reverseWord += wordArr[i];
+    }
+    return reverseWord;
 };
 
 // Do not edit below this line
