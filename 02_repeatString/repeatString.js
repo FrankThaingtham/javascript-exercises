091c9878d4a0537031f12b1ca4cacc573578c2d0
FrankThaingtham <123851541+FrankThaingtham@users.noreply.github.com>
2025-01-02 20:27:21 -0500

    02 complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word, num) {
+    let returnString = "";
+    if (num < 0) {
+        return "ERROR"
+    }
+    for (let i = 0; i < num; i++) {
+        returnString += word;
+    }
+    return returnString;
 };
 
 // Do not edit below this line
